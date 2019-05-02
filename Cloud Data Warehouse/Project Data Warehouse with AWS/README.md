a9c4ee8522559036ddc8f5f4efb4df9dea27724f
nareshk1290 <41761526+nareshk1290@users.noreply.github.com>
2019-05-01 22:50:18 -0400

    Data Warehouse with AWS and Redshift
    Data Warehouse with AWS and Redshift - Udacity - DEND

--- Cloud Data Warehouse/Project Data Warehouse with AWS/README.md
+++ Cloud Data Warehouse/Project Data Warehouse with AWS/README.md
@@ -1,1 +1,106 @@
-Hello
+<b>Introduction</b>
+
+A music streaming startup, Sparkify, has grown their user base and song database and want to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+Task is to build an ETL Pipeline that extracts their data from S3, staging it in Redshift and then transforming data into a set of Dimensional and Fact Tables for their Analytics Team to continue finding Insights to what songs their users are listening to.
+
+<b>Project Description</b>
+
+Application of Data warehouse and AWS to build an ETL Pipeline for a database hosted on Redshift Will need to load data from S3 to staging tables on Redshift and execute SQL Statements that create fact and dimension tables from these staging tables to create analytics
+
+<b>Project Datasets</b>
+
+Song Data Path     -->     s3://udacity-dend/song_data
+Log Data Path      -->     s3://udacity-dend/log_data
+Log Data JSON Path -->     s3://udacity-dend/log_json_path.json
+
+<b>Song Dataset</b>
+
+The first dataset is a subset of real data from the Million Song Dataset(https://labrosa.ee.columbia.edu/millionsong/). Each file is in JSON format and contains metadata about a song and the artist of that song. The files are partitioned by the first three letters of each song's track ID. 
+For example:
+
+song_data/A/B/C/TRABCEI128F424C983.json
+song_data/A/A/B/TRAABJL12903CDCF1A.json
+
+And below is an example of what a single song file, TRAABJL12903CDCF1A.json, looks like.
+
+{"num_songs": 1, "artist_id": "ARJIE2Y1187B994AB7", "artist_latitude": null, "artist_longitude": null, "artist_location": "", "artist_name": "Line Renaud", "song_id": "SOUPIRU12A6D4FA1E1", "title": "Der Kleine Dompfaff", "duration": 152.92036, "year": 0}
+
+<b>Log Dataset</b>
+
+The second dataset consists of log files in JSON format. The log files in the dataset with are partitioned by year and month.
+For example:
+
+log_data/2018/11/2018-11-12-events.json
+log_data/2018/11/2018-11-13-events.json
+
+And below is an example of what a single log file, 2018-11-13-events.json, looks like.
+
+{"artist":"Pavement", "auth":"Logged In", "firstName":"Sylvie", "gender", "F", "itemInSession":0, "lastName":"Cruz", "length":99.16036, "level":"free", "location":"Klamath Falls, OR", "method":"PUT", "page":"NextSong", "registration":"1.541078e+12", "sessionId":345, "song":"Mercy:The Laundromat", "status":200, "ts":1541990258796, "userAgent":"Mozilla/5.0(Macintosh; Intel Mac OS X 10_9_4...)", "userId":10}
+
+<b>Schema for Song Play Analysis</b>
+
+A Star Schema would be required for optimized queries on song play queries
+
+<b>Fact Table</b>
+
+<b>songplays</b> - records in event data associated with song plays i.e. records with page NextSong
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+<b>Dimension Tables</b>
+
+<b>users</b> - users in the app
+user_id, first_name, last_name, gender, level
+
+<b>songs</b> - songs in music database
+song_id, title, artist_id, year, duration
+
+<b>artists</b> - artists in music database
+artist_id, name, location, lattitude, longitude
+
+<b>time</b> - timestamps of records in songplays broken down into specific units
+start_time, hour, day, week, month, year, weekday
+
+<b>Project Template</b>
+
+Project Template include four files:
+
+<b>1. create_table.py</b> is where you'll create your fact and dimension tables for the star schema in Redshift.
+
+<b>2. etl.py</b> is where you'll load data from S3 into staging tables on Redshift and then process that data into your analytics tables on Redshift.
+
+<b>3. sql_queries.py</b> is where you'll define you SQL statements, which will be imported into the two other files above.
+
+<b>4. README.md</b> is where you'll provide discussion on your process and decisions for this ETL pipeline.
+
+<b>Create Table Schema</b>
+
+1. Write a SQL CREATE statement for each of these tables in sql_queries.py
+2. Complete the logic in create_tables.py to connect to the database and create these tables
+3. Write SQL DROP statements to drop tables in the beginning of create_tables.py if the tables already exist. This way, you can run create_tables.py whenever you want to reset your database and test your ETL pipeline.
+4. Launch a redshift cluster and create an IAM role that has read access to S3.
+5. Add redshift database and IAM role info to dwh.cfg.
+6. Test by running create_tables.py and checking the table schemas in your redshift database.
+
+<b>Build ETL Pipeline</b>
+
+1. Implement the logic in etl.py to load data from S3 to staging tables on Redshift.
+2. Implement the logic in etl.py to load data from staging tables to analytics tables on Redshift.
+3. Test by running etl.py after running create_tables.py and running the analytic queries on your Redshift database to compare your results with the expected results.
+4. Delete your redshift cluster when finished.
+
+<b>Final Instructions</b>
+
+1. Import all the necessary libraries
+2. Write the configuration of AWS Cluster, store the important parameter in some other file
+3. Configuration of boto3 which is an AWS SDK for Python
+4. Using the bucket, can check whether files log files and song data files are present
+5. Create an IAM User Role, Assign appropriate permissions and create the Redshift Cluster
+6. Get the Value of Endpoint and Role for put into main configuration file
+7. Authorize Security Access Group to Default TCP/IP Address
+8. Launch database connectivity configuration
+9. Go to Terminal write the command "python create_tables.py" and then "etl.py"
+10. Should take around 4-10 minutes in total
+11. Then you go back to jupyter notebook to test everything is working fine
+12. I counted all the records in my tables
+13. Now can delete the cluster, roles and assigned permission
---
+++ Cloud Data Warehouse/Project Data Warehouse with AWS/sql_queries.py
@@ -0,0 +1,213 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# GLOBAL VARIABLES
+LOG_DATA = config.get("S3","LOG_DATA")
+LOG_PATH = config.get("S3", "LOG_JSONPATH")
+SONG_DATA = config.get("S3", "SONG_DATA")
+IAM_ROLE = config.get("IAM_ROLE","ARN")
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS fact_songplay"
+user_table_drop = "DROP TABLE IF EXISTS dim_user"
+song_table_drop = "DROP TABLE IF EXISTS dim_song"
+artist_table_drop = "DROP TABLE IF EXISTS dim_artist"
+time_table_drop = "DROP TABLE IF EXISTS dim_time"
+
+# CREATE TABLES
+
+staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events
+(
+artist          VARCHAR,
+auth            VARCHAR, 
+firstName       VARCHAR,
+gender          VARCHAR,   
+itemInSession   INTEGER,
+lastName        VARCHAR,
+length          FLOAT,
+level           VARCHAR, 
+location        VARCHAR,
+method          VARCHAR,
+page            VARCHAR,
+registration    BIGINT,
+sessionId       INTEGER,
+song            VARCHAR,
+status          INTEGER,
+ts              TIMESTAMP,
+userAgent       VARCHAR,
+userId          INTEGER
+);
+""")
+
+staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs
+(
+song_id            VARCHAR,
+num_songs          INTEGER,
+title              VARCHAR,
+artist_name        VARCHAR,
+artist_latitude    FLOAT,
+year               INTEGER,
+duration           FLOAT,
+artist_id          VARCHAR,
+artist_longitude   FLOAT,
+artist_location    VARCHAR
+);
+""")
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS fact_songplay
+(
+songplay_id          INTEGER IDENTITY(0,1) PRIMARY KEY sortkey,
+start_time           TIMESTAMP,
+user_id              INTEGER,
+level                VARCHAR,
+song_id              VARCHAR,
+artist_id            VARCHAR,
+session_id           INTEGER,
+location             VARCHAR,
+user_agent           VARCHAR
+);
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS dim_user
+(
+user_id INTEGER PRIMARY KEY distkey,
+first_name      VARCHAR,
+last_name       VARCHAR,
+gender          VARCHAR,
+level           VARCHAR
+);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS dim_song
+(
+song_id     VARCHAR PRIMARY KEY,
+title       VARCHAR,
+artist_id   VARCHAR distkey,
+year        INTEGER,
+duration    FLOAT
+);
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS dim_artist
+(
+artist_id          VARCHAR PRIMARY KEY distkey,
+name               VARCHAR,
+location           VARCHAR,
+latitude           FLOAT,
+longitude          FLOAT
+);
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS dim_time
+(
+start_time    TIMESTAMP PRIMARY KEY sortkey distkey,
+hour          INTEGER,
+day           INTEGER,
+week          INTEGER,
+month         INTEGER,
+year          INTEGER,
+weekday       INTEGER
+);
+""")
+
+# STAGING TABLES
+
+staging_events_copy = ("""
+    COPY staging_events FROM {}
+    CREDENTIALS 'aws_iam_role={}'
+    COMPUPDATE OFF region 'us-west-2'
+    TIMEFORMAT as 'epochmillisecs'
+    TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+    FORMAT AS JSON {};
+""").format(LOG_DATA, IAM_ROLE, LOG_PATH)
+
+staging_songs_copy = ("""
+    COPY staging_songs FROM {}
+    CREDENTIALS 'aws_iam_role={}'
+    COMPUPDATE OFF region 'us-west-2'
+    FORMAT AS JSON 'auto' 
+    TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL;
+""").format(SONG_DATA, IAM_ROLE)
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
+INSERT INTO fact_songplay(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT DISTINCT to_timestamp(to_char(se.ts, '9999-99-99 99:99:99'),'YYYY-MM-DD HH24:MI:SS'),
+                se.userId as user_id,
+                se.level as level,
+                ss.song_id as song_id,
+                ss.artist_id as artist_id,
+                se.sessionId as session_id,
+                se.location as location,
+                se.userAgent as user_agent
+FROM staging_events se
+JOIN staging_songs ss ON se.song = ss.title AND se.artist = ss.artist_name;
+""")
+
+user_table_insert = ("""
+INSERT INTO dim_user(user_id, first_name, last_name, gender, level)
+SELECT DISTINCT userId as user_id,
+                firstName as first_name,
+                lastName as last_name,
+                gender as gender,
+                level as level
+FROM staging_events
+where userId IS NOT NULL;
+""")
+
+song_table_insert = ("""
+INSERT INTO dim_song(song_id, title, artist_id, year, duration)
+SELECT DISTINCT song_id as song_id,
+                title as title,
+                artist_id as artist_id,
+                year as year,
+                duration as duration
+FROM staging_songs
+WHERE song_id IS NOT NULL;
+""")
+
+artist_table_insert = ("""
+INSERT INTO dim_artist(artist_id, name, location, latitude, longitude)
+SELECT DISTINCT artist_id as artist_id,
+                artist_name as name,
+                artist_location as location,
+                artist_latitude as latitude,
+                artist_longitude as longitude
+FROM staging_songs
+where artist_id IS NOT NULL;
+""")
+
+time_table_insert = ("""
+INSERT INTO dim_time(start_time, hour, day, week, month, year, weekday)
+SELECT distinct ts,
+                EXTRACT(hour from ts),
+                EXTRACT(day from ts),
+                EXTRACT(week from ts),
+                EXTRACT(month from ts),
+                EXTRACT(year from ts),
+                EXTRACT(weekday from ts)
+FROM staging_events
+WHERE ts IS NOT NULL;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
